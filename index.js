6d6031d88d54a64fce41ad2fd9ab718a7d1ac49b
mplima08 <93649998+mplima08@users.noreply.github.com>
2022-01-26 14:53:48 -0100

    API

--- index.js
+++ index.js
@@ -9,8 +9,24 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname,'./index.html'))
 })
 
-const port = 3000
 
-app.listen(port, () => {
-    console.log('Listenning...')
+
+
+const mongoose = require('mongoose')
+require('dotenv').config({ path: './private/.env' })
+
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json({ extended: false }))
+
+
+
+mongoose.connect(process.env.MONGOURI, 
+    {useNewUrlParser: true, useUnifiedTopology: true},
+    () => console.log("Connected to MongoDB"));
+
+
+app.listen(4000,(error)=>{
+    if(error) throw error
+    console.log('Api listening on port 4000')
 })
